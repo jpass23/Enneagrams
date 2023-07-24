e4391c6b5305c546644f9087ee278fbf1d595536
jpass23 <jpass23@live.unc.edu>
2023-07-24 14:33:13 -0400

    Frontend created
    Added frontend directory and created the xcode project inside

---
+++ Frontend/Enneagrams/EnneagramsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnneagramsApp.swift
+//  Enneagrams
+//
+//  Created by Jaden Passero on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct EnneagramsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
